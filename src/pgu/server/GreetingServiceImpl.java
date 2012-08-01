ed3621592cb51037d381139a610e3578031d972c
Pascal Guilcher <guilcher.p@sfeir.com>
2012-08-01 13:43:26 +0200

    ..

--- src/pgu/server/GreetingServiceImpl.java
+++ src/pgu/server/GreetingServiceImpl.java
@@ -17,6 +17,7 @@ import org.scribe.oauth.OAuthService;
 import pgu.client.GreetingService;
 import pgu.shared.FieldVerifier;
 
+import com.google.appengine.api.utils.SystemProperty;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 /**
@@ -42,14 +43,14 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements Greetin
         userAgent = escapeHtml(userAgent);
 
         return "Hello, " + input + "!<br><br>I am running " + serverInfo + ".<br><br>It looks like you are using:<br>"
-        + userAgent;
+                + userAgent;
     }
 
     /**
-     * Escape an html string. Escaping data received from the client helps to
-     * prevent cross-site script vulnerabilities.
+     * Escape an html string. Escaping data received from the client helps to prevent cross-site script vulnerabilities.
      * 
-     * @param html the html string to escape
+     * @param html
+     *            the html string to escape
      * @return the escaped string
      */
     private String escapeHtml(final String html) {
@@ -72,18 +73,19 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements Greetin
             throw new RuntimeException(e);
         }
 
-
         final String API_KEY = props.getProperty("api.key");
         final String API_SECRET = props.getProperty("api.secret");
         System.out.println("key " + API_KEY);
         System.out.println("sec " + API_SECRET);
 
         // build the service to make the access token request and all other requests
-        final OAuthService service = new ServiceBuilder()
-        .provider(LinkedInApi.class)
-        .apiKey(API_KEY)
-        .apiSecret(API_SECRET)
-        .build();
+        final OAuthService service = new ServiceBuilder() //
+                .provider(LinkedInApi.class) //
+                .apiKey(API_KEY) //
+                .apiSecret(API_SECRET) //
+                .callback(getCallbackUrl()) //
+                .build();
+
         // set the scanner to catch input from the user
         final Scanner in = new Scanner(System.in);
 
@@ -95,6 +97,10 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements Greetin
         System.out.println("Visit the above URL in your browser and then paste the numerical verifier code here");
         System.out.print(">>");
 
+        // https://developer.linkedin.com/documents/common-issues-oauth-authentication
+        // https://developer.linkedin.com/documents/debugging-api-calls
+        // https://developer.linkedin.com/oauth-test-console
+
         // build the verifer with the pin the user enters
         final Verifier verifier = new Verifier(in.nextLine());
 
@@ -103,10 +109,75 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements Greetin
 
         // now make a simple query to make sure our token works
         // we fetch our own profile on linkedin. This query will be explained more on later pages
-        final String url = "http://api.linkedin.com/v1/people/~";
-        final OAuthRequest request = new OAuthRequest(Verb.GET, url);
-        service.signRequest(accessToken, request);
-        final Response response = request.send();
+        final String myProfileUrl = "http://api.linkedin.com/v1/people/~";
+        OAuthRequest request = newRequest(service, accessToken, myProfileUrl);
+        Response response = request.send();
         System.out.println(response.getBody());
+
+        final String connectionsUrl = "http://api.linkedin.com/v1/people/~/connections";
+        request = newRequest(service, accessToken, connectionsUrl);
+        response = request.send();
+        showResponseCode(response);
+        System.out.println(response.getBody());
+
+        // https://developer.linkedin.com/documents/profile-fields
+        // first-name
+        // last-name
+        // location:(name)
+        // location:(country:(code))
+        // distance
+        // connections
+        // num-connections
+        // num-connections-capped
+        // languages
+
+        // https://developer.linkedin.com/documents/field-selectors
+        // http://api.linkedin.com/v1/people/~:(id,first-name,last-name,industry)
+        // http://api.linkedin.com/v1/people/~/connections:(id,first-name,last-name,industry)
+
+        // https://developer.linkedin.com/documents/connections-api
+        // http://api.linkedin.com/v1/people/~/connections
+        // http://api.linkedin.com/v1/people/id=12345/connections
+        // http://api.linkedin.com/v1/people/~/connections:(headline,first-name,last-name)
+
+        // token invalidation
+        // https://api.linkedin.com/uas/oauth/invalidateToken
+    }
+
+    private String getCallbackUrl() {
+        return isEnvProd() ? "http://pgu-contacts.appspot.com"
+                : "http://127.0.0.1:8888/Pgu_contacts.html?gwt.codesvr=127.0.0.1:9997";
+    }
+
+    public boolean isEnvProd() {
+        return SystemProperty.environment.value() == SystemProperty.Environment.Value.Production;
+    }
+
+    private void showResponseCode(final Response response) {
+        final int responseNumber = response.getCode();
+
+        if (responseNumber >= 199 && responseNumber < 300) {
+            System.out.println("HOORAY IT WORKED!!");
+            System.out.println(response.getBody());
+        } else if (responseNumber >= 500 && responseNumber < 600) {
+            // you could actually raise an exception here in your own code
+            System.out.println("Ruh Roh application error of type 500: " + responseNumber);
+            System.out.println(response.getBody());
+        } else if (responseNumber == 403) {
+            System.out.println("A 403 was returned which usually means you have reached a throttle limit");
+        } else if (responseNumber == 401) {
+            System.out.println("A 401 was returned which is a Oauth signature error");
+            System.out.println(response.getBody());
+        } else {
+            System.out.println("We got a different response that we should add to the list: " + responseNumber);
+            System.out.println(response.getBody());
+        }
+    }
+
+    private OAuthRequest newRequest(final OAuthService service, final Token accessToken, final String myProfileUrl) {
+        final OAuthRequest request = new OAuthRequest(Verb.GET, myProfileUrl);
+        request.addHeader("x-li-format", "json");
+        service.signRequest(accessToken, request);
+        return request;
     }
 }
