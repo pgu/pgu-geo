9afa2f11c8951a410848da2792e64742fa44baaf
Pascal Guilcher <guilcher.p@sfeir.com>
2012-08-14 08:40:02 +0200

    ..

--- src/pgu/client/app/AppState.java
+++ src/pgu/client/app/AppState.java
@@ -1,13 +1,24 @@
 package pgu.client.app;
 
+import pgu.shared.dto.RequestToken;
 
 public class AppState {
+
+    private RequestToken requestToken;
 
     public AppState() {
     }
 
     public boolean hasUser() {
         return false;
+    }
+
+    public RequestToken getRequestToken() {
+        return requestToken;
+    }
+
+    public void setRequestToken(final RequestToken requestToken) {
+        this.requestToken = requestToken;
     }
 
 }
--- src/pgu/client/oauth/OAuthActivity.java
+++ src/pgu/client/oauth/OAuthActivity.java
@@ -1,7 +1,10 @@
 package pgu.client.oauth;
 
 import pgu.client.app.mvp.ClientFactory;
+import pgu.client.app.utils.AsyncCallbackApp;
 import pgu.client.app.utils.ClientUtils;
+import pgu.client.service.LinkedinServiceAsync;
+import pgu.shared.dto.OauthAuthorizationStart;
 
 import com.google.gwt.activity.shared.AbstractActivity;
 import com.google.gwt.event.shared.EventBus;
@@ -10,17 +13,19 @@ import com.google.gwt.user.client.ui.AcceptsOneWidget;
 
 public class OAuthActivity extends AbstractActivity implements OAuthPresenter {
 
-    private final Place         redirectPlace;
-    private final ClientFactory clientFactory;
-    private final OAuthView     view;
-    private final ClientUtils   u = new ClientUtils();
+    private final Place                redirectPlace;
+    private final ClientFactory        clientFactory;
+    private final OAuthView            view;
+    private final ClientUtils          u = new ClientUtils();
+    private final LinkedinServiceAsync linkedinService;
 
-    private EventBus            eventBus;
+    private EventBus                   eventBus;
 
     public OAuthActivity(final Place place, final ClientFactory clientFactory) {
         redirectPlace = place;
         this.clientFactory = clientFactory;
         view = clientFactory.getOAuthView();
+        linkedinService = clientFactory.getLinkedinService();
     }
 
     @Override
@@ -28,6 +33,16 @@ public class OAuthActivity extends AbstractActivity implements OAuthPresenter {
         this.eventBus = eventBus;
         view.setPresenter(this);
         panel.setWidget(view.asWidget());
+
+        linkedinService.getLinkedinUrlAuthorization(new AsyncCallbackApp<OauthAuthorizationStart>(eventBus) {
+
+            @Override
+            public void onSuccess(final OauthAuthorizationStart oas) {
+                clientFactory.getAppState().setRequestToken(oas.getRequestToken());
+                // add to display an iframe or a link with the oas.getAuthorizationUrl();
+            }
+
+        });
     }
 
 }
