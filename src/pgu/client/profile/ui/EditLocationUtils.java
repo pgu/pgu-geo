aedbbabb6d5ecd8f2b69ca3d55ed285c90e8fcc0
Pascal Guilcher <guilcher.p@sfeir.com>
2012-09-14 15:16:51 +0200

    ..

--- src/pgu/client/profile/ui/EditLocationUtils.java
+++ src/pgu/client/profile/ui/EditLocationUtils.java
@@ -12,7 +12,22 @@ public class EditLocationUtils {
         }
     }
 
-    public static native void showLatitudeAndLongitude(final EditLocationViewImpl editLocationViewImpl, final String locationName) /*-{
+    public static native void showLatitudeAndLongitude(final EditLocationViewImpl view, final String locationName) /*-{
+
+        var lat,lng;
+
+        var geopoint_is_available = @pgu.client.app.utils.LocationsUtils::isLocationInReferential(Ljava/lang/String;)(location_name);
+        if (geopoint_is_available) {
+
+            var geopoint = @pgu.client.app.utils.LocationsUtils::getGeopoint(Ljava/lang/String;)(location_name);
+            lat = geopoint.lat;
+            lng = geopoint.lng;
+
+        } else {
+            lat = lng = '';
+
+        }
+        view.@pgu.client.profile.ui.EditLocationViewImpl::showLatitudeAndLongitude(Ljava/lang/String;Ljava/lang/String;)(lat,lng);
 
     }-*/;
 
