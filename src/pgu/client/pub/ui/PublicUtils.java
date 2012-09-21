0d7e28a0021157a1c95eaa588eab258bec410e47
Guilcher Pascal <pgu.dev@gmail.com>
2012-09-21 21:19:42 +0200

    ..

---
+++ src/pgu/client/pub/ui/PublicUtils.java
@@ -0,0 +1,35 @@
+package pgu.client.pub.ui;
+
+public class PublicUtils {
+
+    public static native void initProfileMap() /*-{
+
+        var div = $wnd.document.getElementById("pgu_geo_public_profile_map");
+
+        if (div === null) {
+            $wnd.setTimeout(
+                function() {
+                    @pgu.client.pub.ui.PublicUtils::initProfileMap()();
+                }
+                , 1000
+            );
+            return;
+        }
+
+        var
+            google = $wnd.google
+          , mapOptions = {
+              zoom: 2,
+              center: new google.maps.LatLng(0, 0),
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+        ;
+
+        $wnd.pgu_geo.public_profile_map = new google.maps.Map( //
+            div //
+            , mapOptions);
+
+    }-*/;
+
+
+}
